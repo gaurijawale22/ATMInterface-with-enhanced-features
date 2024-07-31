ee7a28dc4d3afcdca1967de90cb717b179960462
gaurijawale22 <154775597+gaurijawale22@users.noreply.github.com>
2024-07-31 13:20:45 +0530

    Update SavingsAccount.java

--- src/account/SavingsAccount.java
+++ src/account/SavingsAccount.java
@@ -1,1 +1,15 @@
+public class SavingsAccount extends Account {
+    private double interestRate;
+
+    public SavingsAccount(String accountNumber, double initialBalance, double interestRate) {
+        super(accountNumber, initialBalance);
+        this.interestRate = interestRate;
+    }
+
+    public double getInterestRate() {
+        return interestRate;
+    }
+
+    // Other SavingsAccount methods...
+}
 
